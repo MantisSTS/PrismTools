5b83ab64260aad41a133fb69e129eb420fd11cab
MantisSTS <rclifford@cybershade.org>
2023-01-12 10:50:23 +0000

    Adding file with prism struct data

---
+++ prism-data-structs.go
@@ -0,0 +1,69 @@
+// Just to stop erroring
+package main
+
+type Prism struct {
+	Issues  []Issue `json:"issues"`
+	Version int64   `json:"version"`
+	Phase   Phase   `json:"phase"`
+}
+
+type Phase struct {
+	ApprovedBy       *interface{} `json:"approved_by"`
+	ApprovedDate     *interface{} `json:"approved_date"`
+	Caveat           string       `json:"caveat"`
+	CompletedBy      *interface{} `json:"completed_by"`
+	EndDate          string       `json:"end_date"`
+	ExecutiveSummary string       `json:"executive_summary"`
+	Location         string       `json:"location"`
+	Name             string       `json:"name"`
+	QaStatus         string       `json:"qa_status"`
+	ScopeSummary     string       `json:"scope_summary"`
+	StartDate        string       `json:"start_date"`
+	Status           string       `json:"status"`
+	TestType         string       `json:"test_type"`
+	Tester           string       `json:"tester"`
+}
+
+type Issue struct {
+	AffectedHosts           []AffectedHost `json:"affected_hosts"`
+	Assignee                *string        `json:"assignee"`
+	Assignees               *[]string      `json:"assignees"`
+	ClientDefinedRiskRating *string        `json:"client_defined_risk_rating"`
+	ConfirmedAt             string         `json:"confirmed_at"`
+	Cves                    *[]string      `json:"cves"`
+	CvssVector              *string        `json:"cvss_vector"`
+	ExploitAvailable        *bool          `json:"exploit_available"`
+	Finding                 string         `json:"finding"`
+	Id                      *int64         `json:"id"`
+	Name                    string         `json:"name"`
+	NessusId                *int           `json:"nessus_id"`
+	OriginalRiskRating      string         `json:"original_risk_rating"`
+	OwaspId                 *string        `json:"owasp_id"`
+	PublishedAt             *string        `json:"published_at"`
+	Rapid7Id                *string        `json:"rapid7_id"`
+	Recommendation          *string        `json:"recommendation"`
+	References              []string       `json:"references"`
+	RemediatedAt            *string        `json:"remediated_at"`
+	Status                  string         `json:"status"`
+	Summary                 *string        `json:"summary"`
+	SuppressForProject      *bool          `json:"suppress_for_project"`
+	SuppressOnAllProjects   *bool          `json:"suppress_on_all_projects"`
+	SuppressUntil           *string        `json:"suppress_until"`
+	TechnicalDetails        string         `json:"technical_details"`
+}
+
+type AffectedHost struct {
+	Cpes                *[]string `json:"cpes"`
+	Hostname            string    `json:"hostname"`
+	Ip                  string    `json:"ip"`
+	Location            *string   `json:"location"`
+	Name                *string   `json:"name"`
+	OperatingSystem     *string   `json:"operating_system"`
+	Port                *int      `json:"port"`
+	Protocol            *string   `json:"protocol"`
+	Service             *string   `json:"service"`
+	Status              *string   `json:"status"`
+	SuppressAllProjects *bool     `json:"suppress_all_projects"`
+	SuppressProject     *bool     `json:"suppress_project"`
+	SuppressUntil       *string   `json:"suppress_until"`
+}
